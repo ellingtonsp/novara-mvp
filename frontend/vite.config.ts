3fef307ea6514f84eb0a1b54be545aa2bbf7bc81
ellingtonsp <ellingtonsp3@gmail.com>
2025-07-22 09:04:30 -0700

    Continued changes to the Vite configuration file to set the base path and resolve alias.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: '/novara-mvp/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
